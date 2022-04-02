78a88563e063fc1954a9fc3286e30e9a22363efb
Javier Palacios <javierpalaciosbotejara@gmail.com>
2022-04-02 12:39:17 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Main } from "./views/Main";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/views/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Main = () => {
+  return (
+    <div className="app">
+      <h1>Make History</h1>
+    </div>
+  );
+};
