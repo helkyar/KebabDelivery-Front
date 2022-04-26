7d8a327b98f999281724fafc16e355d800ac388a
josemsb92 <josemsb92@gmail.com>
2022-04-26 21:01:08 +0200

    stepper imported

--- src/views/StepShop.jsx
+++ src/views/StepShop.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 
+import Stepper from "react-simple-stepper-component";
 export const StepShop = () => {
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState(0);
 
   const managerStepComponent = () => {
     if (step === 1) {
@@ -29,5 +30,27 @@ export const StepShop = () => {
       );
     }
   };
-  return <>{managerStepComponent()}</>;
+  return (
+    <>
+      {managerStepComponent()} 
+      <Stepper
+        lineHeight="3"
+        defaultTitleColor="#787878"
+        defaultColor="#FFFFFF"
+        defaultBorderColor="#FF8454"
+        completeColor="#FF8454"
+        completeBarColor="#FF8454"
+        lineWeight="50"
+        steps={[
+          { title: "destino" },
+          { title: "tamaño" },
+          { title: "fecha" },
+          { title: "resumen" },
+        ]}
+        activeStep={step}
+      />
+      <button onClick={() => setStep(step + 1)}>siguiente</button>{" "}
+      <button onClick={() => setStep(step - 1)}>anterior</button>
+    </>
+  );
 };
