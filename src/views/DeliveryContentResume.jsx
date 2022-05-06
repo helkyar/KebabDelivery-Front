8879afb99998fea87237ecb3420a54bae27ce961
aldo <aldofcastellanop@gmail.com>
2022-05-06 18:26:50 +0200

    update dev

--- src/views/DeliveryContentResume.jsx
+++ src/views/DeliveryContentResume.jsx
@@ -1,12 +1,40 @@
 import { Destination } from "components/destiny/Destination";
-
+import { useState } from "react";
+import {
+  getStorageCart,
+  postStorageCart,
+} from "helpers/localStorage/storageCart";
 export const DeliveryContentResume = ({ props }) => {
+  const [basket, setBasket] = useState(getStorageCart());
   console.log(props);
   return (
     <>
       <h1>model de resumen</h1>
-
-      <button onClick={props.back} className="button button-destination">Atrás</button>
+      <div>
+        <h4>direccion</h4>
+        <input
+          type="text"
+          value={basket.from}
+          className="input-primary input-resumen"
+        />
+        <input
+          type="text"
+          value={basket.to}
+          className="input-primary input-resumen"
+        />
+      </div>
+      <div>
+        <h4>fecha</h4>
+        <input type="date" className="input-primary input-resumen" />
+        <input type="time" className="input-primary input-resumen" />
+      </div>
+      <div>
+        <h4>fecha</h4>
+        <textarea className="input-primary textarea-resumen" />
+      </div>
+      <button onClick={props.back} className="button button-destination">
+        Atrás
+      </button>
     </>
   );
 };
