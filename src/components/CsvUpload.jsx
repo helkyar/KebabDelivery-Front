1a33cbbc68af337e3ae85462895d6b9d871b8bf7
josemsb92 <josemsb92@gmail.com>
2022-05-20 19:25:32 +0200

    Csv download link

--- src/components/CsvUpload.jsx
+++ src/components/CsvUpload.jsx
@@ -1,13 +1,14 @@
 import postOrder from "helpers/orders/postOrder";
-import sendEmail from "helpers/orders/sendEmail"
+import sendEmail from "helpers/orders/sendEmail";
 import { useSession } from "helpers/session/useSession";
 import Papa from "papaparse";
 import { useEffect } from "react";
 import { useRef, useState } from "react";
+import { CSVLink } from "react-csv";
 
 export const CsvUpload = () => {
   const [csvfile, setCsvfile] = useState();
-  const {jwt, isLogged, user} = useSession();
+  const { jwt, isLogged, user } = useSession();
   const fileInput = useRef(null);
 
   const handleChange = (event) => {
@@ -18,38 +19,34 @@ export const CsvUpload = () => {
     Papa.parse(csvfile, {
       complete: (results) => setCsvfile(results),
     });
-    
   };
 
-  useEffect(()=>{
-    
-    if(!isLogged){
-// open modal
+  useEffect(() => {
+    if (!isLogged) {
+      // open modal
       return;
     }
-    if(!csvfile?.data || user?.email) return;
-    csvfile.data.forEach(async(order,i) => {
+    if (!csvfile?.data || user?.email) return;
+    csvfile.data.forEach(async (order, i) => {
       console.log(order);
-      if(i===0) return; //header
-      if(order.length < 8) return;
-      const pakage = { 
-        from: order[0], 
-        to: order[1], 
-        id_delivered: order[2], 
-        pick_up_date: order[3], 
-        pick_up_time: order[4],    
-        pakage: order[5],    
-        letter: order[6],    
-        comment: order[7],
+      if (i < 2) return; //header
+      if (order.length < 7) return;
+      const pakage = {
+        from: order[0],
+        to: order[1],
+        pick_up_date: new Date(order[2]),
+        pakage: order[3],
+        letter: order[4],
+        comment: order[5],
       };
-    const data = await postOrder(pakage, jwt)
-     const email = {
-      to: user.email, 
-      text: `Tu paquete ha sido registrado correctamente, usa el siguiente código para hacer el seguimiento ${data.id}`
-    }
+      const data = await postOrder(pakage, jwt);
+      const email = {
+        to: user.email,
+        text: `Tu paquete ha sido registrado correctamente, usa el siguiente código para hacer el seguimiento ${data.id}`,
+      };
       // await sendEmail(email)
     });
-  },[csvfile, user])
+  }, [csvfile, user]);
 
   return (
     <div className="csv-form">
@@ -60,8 +57,30 @@ export const CsvUpload = () => {
         name="file"
         placeholder={null}
         onChange={handleChange}
-      />      
+      />
       <button onClick={importCSV}> Process File! </button>
+      <CSVLink
+        data={[
+          {
+            from: "Departure Street",
+            to: "Destinatary Street",
+            pick_up_date: "YYYY-MM-DDT00:00",
+            package: "S, M , L",
+            letter: "S, M , L",
+            comment: "Leave a comment here",
+          },
+        ]}
+        headers={[
+          { label: "From", key: "from" },
+          { label: "To", key: "to" },
+          { label: "Pick up Date", key: "pick_up_date" },
+          { label: "Package", key: "package" },
+          { label: "Letter ", key: "letter" },
+          { label: "Comment ", key: "comment" },
+        ]}
+      >
+        Download CSV Template
+      </CSVLink>
     </div>
   );
 };
