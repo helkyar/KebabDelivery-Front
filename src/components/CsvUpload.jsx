c0557853e1aadc54892edf7c72b417aec32b0222
aldo <aldofcastellanop@gmail.com>
2022-05-20 18:39:04 +0200

    create component ModalSession

--- src/components/CsvUpload.jsx
+++ src/components/CsvUpload.jsx
@@ -1,5 +1,5 @@
 import postOrder from "helpers/orders/postOrder";
-import sendEmail from "helpers/orders/sendEmail"
+import sendEmail from "helpers/orders/sendEmail";
 import { useSession } from "helpers/session/useSession";
 import Papa from "papaparse";
 import { useEffect } from "react";
@@ -7,7 +7,7 @@ import { useRef, useState } from "react";
 
 export const CsvUpload = () => {
   const [csvfile, setCsvfile] = useState();
-  const {jwt, isLogged, user} = useSession();
+  const { jwt, isLogged, user } = useSession();
   const fileInput = useRef(null);
 
   const handleChange = (event) => {
@@ -18,38 +18,36 @@ export const CsvUpload = () => {
     Papa.parse(csvfile, {
       complete: (results) => setCsvfile(results),
     });
-    
   };
 
-  useEffect(()=>{
-    
-    if(!isLogged){
-// open modal
+  useEffect(() => {
+    if (!isLogged) {
+      // open modal
       return;
     }
-    if(!csvfile?.data || user?.email) return;
-    csvfile.data.forEach(async(order,i) => {
+    if (!csvfile?.data || user?.email) return;
+    csvfile.data.forEach(async (order, i) => {
       console.log(order);
-      if(i===0) return; //header
+      if (i === 0) return; //header
       // id_client
-      const pakage = { 
-        from: order[1], 
-        to: order[2], 
-        id_delivered: order[3], 
-        pick_up_date: order[4], 
-        pick_up_time: order[5],    
-        pakage: order[6],    
-        letter: order[7],    
+      const pakage = {
+        from: order[1],
+        to: order[2],
+        id_delivered: order[3],
+        pick_up_date: order[4],
+        pick_up_time: order[5],
+        pakage: order[6],
+        letter: order[7],
         comment: order[8],
       };
-    const data = await postOrder(pakage, jwt)
-     const email = {
-      to: user.email, 
-      text: `Tu paquete ha sido registrado correctamente, usa el siguiente código para hacer el seguimiento ${data.id}`
-    }
-      await sendEmail(email)
+      const data = await postOrder(pakage, jwt);
+      const email = {
+        to: user.email,
+        text: `Tu paquete ha sido registrado correctamente, usa el siguiente código para hacer el seguimiento ${data.id}`,
+      };
+      await sendEmail(email);
     });
-  },[csvfile, user])
+  }, [csvfile, user]);
 
   return (
     <div className="csv-form">
@@ -60,7 +58,7 @@ export const CsvUpload = () => {
         name="file"
         placeholder={null}
         onChange={handleChange}
-      />      
+      />
       <button onClick={importCSV}> Process File! </button>
     </div>
   );
